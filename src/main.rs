14d6baa41091df9467e56af5b5ca3f476e89589b
Ophir LOJKINE <o.lojkine@qwant.com>
2018-09-14 11:41:10 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+extern crate nom_sql;
+
+use std::io::{self, BufRead};
+use nom_sql::{
+    parser::parse_query,
+    SqlQuery::Insert,
+    InsertStatement,
+    Table,
+    Literal
+};
+
+fn extract_data(input: &str) -> Option<Vec<Vec<Literal>>> {
+    match parse_query(input) {
+        Ok(Insert(InsertStatement {
+                table: Table {name, ..},
+                data, ..
+        })) => if name == "externallinks" {Some(data)} else {None},
+        parsed => {
+            eprintln!("Not a valid import statement: {:?}", parsed);
+            None
+        }
+    }
+}
+
+fn process_line(input: &str) -> Vec<String> {
+    extract_data(input).iter()
+        .flat_map(|data| data.iter())
+        .flat_map(|v| match v.get(2) {
+            Some(Literal::String(s)) => Some(s.clone()),
+            _ => None
+        })
+        .collect()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line_str = &*line.unwrap();
+        for url in process_line(line_str) {
+            println!("{}", url);
+        }
+    }
+}
